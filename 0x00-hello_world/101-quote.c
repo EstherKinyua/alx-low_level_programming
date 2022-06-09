fe5d9a25a2c78d00223f5a927abfb9bbb97eb652
EstherKinyua <estherkinyua054@gmail.com>
2022-06-09 07:10:25 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - program that prints exactly and that
- * piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to the standard error."
- * Return : 1
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ * followed by new line, to the standard error. 
+ * Return: Always 0 (success)
  */
 int main(void)
 {

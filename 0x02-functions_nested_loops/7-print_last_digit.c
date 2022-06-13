d36cb45ca85fa6732292f46a1f9cedb93e702b2b
EstherKinyua <estherkinyua054@gmail.com>
2022-06-13 06:17:41 -0700

    print

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit -> prints last digit of a number
- * @n: a parameter
+ * @n: the passed args
  * Return: last digit
  */
 int print_last_digit(int n)
@@ -13,5 +13,5 @@ int print_last_digit(int n)
 		n = -n;
 	x = n % 10;
 	_putchar(x + '0');
-	return (n % 10);
+	return (x);
 }

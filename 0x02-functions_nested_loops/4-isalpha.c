11d7d672b67371bdc0ad1f55309b2506a8a46079
EstherKinyua <estherkinyua054@gmail.com>
2022-06-13 05:44:58 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha -> checks if characters have alphabetic order
+ * @c: a character argument
+ * Return: Returns 1 and 0 depending on condition
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
